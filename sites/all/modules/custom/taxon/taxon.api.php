6f3c7e9b9d1eb7898bcecdbd7d9f1d8180e8a290
Andreas Loo <andreas@gaup.se>
2018-02-14 12:20:13 +0100

    Replace server-side HTTP queries with direct database queries in taxon module(s)

--- sites/all/modules/custom/taxon/taxon.api.php
+++ sites/all/modules/custom/taxon/taxon.api.php
@@ -11,14 +11,34 @@
  */
 
 /**
+ * Act on a taxon being loaded from database.
+ *
+ * The module may add additional information to the taxon object.
+ *
+ * @see taxon_external_link_taxon_load()
+ * @see taxon_facts_taxon_load()
+ * @see taxon_media_taxon_load()
+ * @see taxon_navigation_taxon_load()
+ * @see taxon_synonym_taxon_load()
+ *
+ * @param $taxon
+ *   The taxon that is being loaded.
+ */
+ function hook_taxon_load($taxon) {
+   if (!isset($taxon->additional_information)) {
+     $taxon->additional_information = $additional_information;
+   }
+ }
+
+/**
  * Act on a taxon before rendering.
- * 
+ *
  * The module may add elements to $taxon->content prior to rendering. The structure
  * of $taxon->content is a renderable array as expected by drupal_render().
- * 
+ *
  * @see taxon_facts_taxon_view()
  * @see taxon_media_taxon_view()
- * 
+ *
  * @param $taxon
  *   The taxon that is being assembled for rendering.
  */
